c869a8fab836175d9d40d47278f5ae6e83d70fe7
aragaer <aragaer@gmail.com>
2009-04-29 16:11:59 +0400

    inp3 is now passed

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
 CFLAGS += -O1
 all: bact
-
+spec: bact.c
+	$(CC) bact.c -o bact -O1 -DONLINE_JUDGE
--- bact.c
+++ bact.c
@@ -14,66 +14,78 @@
 #define MAX_QS (MAX_SIZE-2)*(MAX_SIZE-2)
 
 #define cell_t char
-#define MAP_SIZE (MAX_SIZE*MAX_SIZE*sizeof(cell_t))
+#define MAP_SIZE (copied_area*sizeof(cell_t))
 
-static cell_t memory_pool[MAX_QS][MAX_SIZE*MAX_SIZE];
+static cell_t memory_pool[MAX_QS][(MAX_SIZE+2)*(MAX_SIZE+2)];
+static char counted_already[(MAX_SIZE+2)*(MAX_SIZE+2)];
 
-static int cols, rows, area;
+static int cols, rows, area, copied_area, rowlen;
 static int nqs = 0, n5s, qs[MAX_QS];
-static char *solution;
+static char solution[2*MAX_SIZE*MAX_SIZE];
 
 #ifndef ONLINE_JUDGE
 static void print_out(cell_t *map) {
     int i, j;
-    for (i = 0; i < rows; i++) {
-        for (j = 0; j < cols; j++)
-            printf("%d ", map[i*cols + j]);
+    for (i = 0; i < rows+2; i++) {
+        for (j = 0; j < cols+2; j++)
+            printf("%d ", map[i*rowlen + j]);
         printf("\n");
     }
 }
 #endif
 
 /* Remove a bacteria from a neighbour cell */
-static inline int dec_bacteria(cell_t *mp) {
-    switch (*mp) {
-    case 1:
-        return -1;
-    default:
-        (*mp)--;
-    case 0:
+static int dec_bacteria(cell_t *mp) {
+    if (*mp == 0)
         return 0;
-    }
+    if (*mp == 1)
+        return -1;
+    (*mp)--;
+    return 0;
 }
 
 /* Revert a placement of bacteria */
-static inline int untake_bacteria(cell_t *map, int pos) {
-    int x = pos % cols;
-    int y = pos / cols;
-    map[pos] = 0;
-    return (( x > 0      && dec_bacteria(map + pos - 1   ))
-        ||  ( y > 0      && dec_bacteria(map + pos - cols))
-        ||  ( x < cols-1 && dec_bacteria(map + pos + 1   ))
-        ||  ( y < rows-1 && dec_bacteria(map + pos + cols)));
+static int untake_bacteria(cell_t *mp) {
+    *mp = 0;
+    return (dec_bacteria(mp - 1)
+        ||  dec_bacteria(mp - rowlen)
+        ||  dec_bacteria(mp + 1)
+        ||  dec_bacteria(mp + rowlen));
 }
 
-/* This is one step of recursion */
-static int do_step(int used1s, int sdepth, int last_qnum) {
-    int i, p;
-    cell_t *map = memory_pool[used1s], *new_map = memory_pool[used1s+1];
-    int local_depth = sdepth;
+/* check if number of non-zero neighbours is too low */
+static int low_neigh_count(cell_t *mp) {
+    return *mp > (mp[-1]!=0) + (mp[1]!=0) + (mp[rowlen]!=0) + (mp[-rowlen]) + 1;
+}
 
-    for (i = 0; i < area; i++) {
-        if (map[i] != 1)
+/* This is one step of recursion turning 5s into 1s*/
+static int do_step5to1(int used1s, int sdepth, int last_qnum) {
+    int i, pq;
+    cell_t *map = memory_pool[used1s], *new_map = memory_pool[used1s+1], *mp;
+    int local_depth = sdepth;
+    memset(counted_already+rowlen, copied_area, 0);
+    for (i = rowlen + 1; i < copied_area + rowlen - 1; i++) {
+        mp = map + i;
+        switch (*mp) {
+        case 0:
             continue;
-        if (untake_bacteria(map, i))
-            return -1;
-        solution[local_depth*2] =     i % cols + 1;
-        solution[local_depth*2 + 1] = i / cols + 1;
-        local_depth++;
-        if (i > cols && map[i - cols] == 1)
-            i -= cols + 1;
-        else if (i > 0 && map[i - 1] == 1)
-            i -= 2;
+        case 1:
+            if (untake_bacteria(mp))
+                return -1;
+            solution[local_depth*2] =     i % rowlen;
+            solution[local_depth*2 + 1] = i / rowlen;
+            local_depth++;
+            if (mp[-rowlen] == 1)
+                i -= rowlen + 1;
+            else if (mp[-1] == 1)
+                i -= 2;
+            break;
+        default:
+            if (!counted_already[i] && low_neigh_count(mp))
+                return -1;
+            counted_already[i] = 1;
+            break;
+        }
     }
 
 /* check the results */
@@ -86,18 +98,27 @@ static int do_step(int used1s, int sdepth, int last_qnum) {
 #endif
 /* We did everything we can do without assigning another non-5 cell */
     local_depth++;
-    for (p = last_qnum + 1; p < n5s + used1s + 1; p++) {
-        i = qs[p];
+    for (pq = last_qnum + 1; pq < n5s + used1s + 1; pq++) {
+        i = qs[pq];
         if (map[i] != 5)
             continue;
+        if ((memory_pool[0][i-1] == 5 && map[i-1] == 0)
+                || (memory_pool[0][i-rowlen] == 5 && map[i-rowlen] == 0))
+            continue; /* one of elder questionable neigbours is already turned into 1 */
 
-        memcpy(new_map, map, MAP_SIZE);
+        memcpy(new_map+rowlen, map+rowlen, copied_area);
 
-        solution[local_depth*2 - 2] = i % cols + 1;
-        solution[local_depth*2 - 1] = i / cols + 1;
-        if (untake_bacteria(new_map, i) == 0
-                && do_step(used1s+1, local_depth, p) == 0) /* This worked */
+        solution[local_depth*2 - 2] = i % rowlen;
+        solution[local_depth*2 - 1] = i / rowlen;
+        debug(("Assigning 1 to position %d\n", i));
+        if (untake_bacteria(new_map + i) == 0
+                && do_step5to1(used1s+1, local_depth, pq) == 0) {/* This worked */
             return 0;
+        }
+        debug(("Assigning 1 to position %d failed\n", i));
+        if ((memory_pool[0][i-1] == 5 && map[i-1] != 0)
+                || (memory_pool[0][i-rowlen] == 5 && map[i-rowlen] != 0))
+            return -1; /* one of elder questionable neigbours is already not 1 */
     }
 
     return -1;
@@ -108,19 +129,21 @@ int main() {
     cell_t *map = memory_pool[0];
 
     scanf("%d%d", &rows, &cols);
+    rowlen = cols + 2;
     area = cols*rows;
+    copied_area = rowlen*rows;
 
-    solution = malloc(area*sizeof(char));
-
-    for (i = 0, pos = 0; i < rows; i++)
-        for (j = 0; j < cols; j++, pos++) {
+    for (i = 1, pos = rowlen + 1; i <= rows; i++) {
+        for (j = 1; j <= cols; j++, pos++) {
             scanf("%d", map + pos);
             sum += map[pos];
-            if (i && j && i < rows-1 && j < cols-1 && map[pos] == 1) {
+            if (i>1 && j>1 && i < rows && j < cols && map[pos] == 1) {
                 qs[nqs++] = pos;
                 map[pos] = 5;
             }
         }
+        pos+=2;
+    }
 
     n5s = area*3 - cols - rows - sum;
 #ifdef ONLINE_JUDGE /* short-circuit check */
@@ -132,7 +155,7 @@ int main() {
     n5s /= 4;
     debug(("We've got %d potential 5s, %d real ones\n", nqs, n5s));
 
-    if (do_step(0, 0, -1)) {
+    if (do_step5to1(0, 0, -1)) {
         printf("No\n");
     } else {
         printf("Yes\n");

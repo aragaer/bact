bd3a7ce0c4a858e12c4a39f0edb1e9193160931a
aragaer <aragaer@gmail.com>
2009-05-08 19:10:05 +0400

    WIP

--- yaab.c
+++ yaab.c
@@ -26,13 +26,18 @@ struct level {
     int level;
     cell_t map[MAX_AREA_W_SAFE];
     int solution_depth;
-    int lor_num, early_count;
+    int position, tension1, tension2, surface1, surface2;
 } static start[MAX_LEVELS];
 
 static int i[MAX_AREA], j[MAX_AREA];
 static int up[MAX_AREA], down[MAX_AREA], left[MAX_AREA], right[MAX_AREA];
 static int solution[MAX_AREA];
-static int qs[MAX_LEVELS];
+struct q {
+    int pos;
+    struct q *next;
+} static qs[MAX_LEVELS];
+
+const int f[] = {-4, 4, 2, 0, -2, -4};
 
 static int cols, rows, area, n5s, nqs;
 
@@ -42,7 +47,7 @@ static void print_out(cell_t *map) {
     int i, j;
     for (i = 0; i < rows; i++) {
         for (j = 0; j < cols; j++) {
-            printf("%d", map[i*cols + j]);
+            printf("%d ", map[i*cols + j]);
         }
         printf("\n");
     }
@@ -101,7 +106,41 @@ static inline int unmake(struct level *level) {
     return local_depth != area;
 }
 
-static int solve(struct level *level) {
+static int analyze(struct level *level) {
+    int i, j, pos, qnum, tension1 = 0, tension2 = level->tension2;
+    cell_t *map = level->map;
+
+    print_out(map);
+    /* first line */
+    for (pos = 0; pos < cols; pos++) {
+        tension1 += f[map[pos]];
+    }
+    tension1 -= cols + 2;
+
+    tension2 -= tension1;
+    
+    for (i = 1; i < rows - 1; i++) {
+        debug(("Starting line %d\n", i+1));
+        debug(("Tension of the first %d lines: %d\n", i, tension1));
+        debug(("Tension of the last %d lines: %d\n", rows - i, tension2));
+        tension1 += f[map[pos]] - 1;
+        tension2 -= f[map[pos]] + f[map[pos+1]] - 1;
+        pos++;
+        debug(("Tension high: %d, tension low: %d, tension mid: %d\n", tension1, tension2, f[map[pos]]));
+        for (j = 1; j < cols - 1; j++, pos++) {
+            debug(("Looking at %d in position %d:%d\n", map[pos], i+1, j+1));
+            debug(("High: %d to %d 5s\n", -((cols + 1 - tension1) >> 3), (cols + 1 + tension1) >> 3));
+            debug(("Low:  %d to %d 5s\n", -((cols + 1 - tension2) >> 3), (cols + 1 + tension2) >> 3));
+            debug(("Tension high: %d, tension low: %d, tension mid: %d\n", tension1, tension2, f[map[pos]]));
+            tension1 += f[map[pos]];
+            tension2 -= f[map[pos+1]];
+        }
+        tension1 += f[map[pos]] + f[map[pos+1]] - 1;
+        tension2 -= f[map[pos+1]] + 1;
+        pos++;
+        debug(("Finished line %d. ", i+1));
+    }
+
     return -1;
 }
 
@@ -125,11 +164,13 @@ int main() {
         scanf("%d", start->map + pos);
         sum += start->map[pos];
         if (start->map[pos] == 1 && i[pos] && j[pos] && i[pos] < rows-1 && j[pos] < cols-1) {
-            start->map[pos] = 5;
-            qs[lor_count++] = pos;
+            qs[nqs].pos = pos;
+            qs[nqs].next = qs + nqs + 1;
+            nqs++;
             debug(("%d,", pos));
         }
     }
+    qs[nqs-1].next = NULL;
     debug(("\b \n"));
 
     start->level = 0;
@@ -137,14 +178,16 @@ int main() {
 
     n5s = area*3 - cols - rows - sum;
 #ifdef ONLINE_JUDGE /* short-circuit check */
-    if (real_deathcount%4) {
+    if (n5s%4) {
         printf("No\n");
         return 0;
     }
 #endif
     n5s /= 4;
 
-    if (solve(start) == 0) {
+    start->tension2 = n5s*8;
+
+    if (analyze(start) == 0) {
         printf("Yes\n");
         for (pos = area; pos--; )
             printf("%d %d\n", solution[pos] / cols + 1, solution[pos] % cols + 1);
